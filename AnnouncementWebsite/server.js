3ff121d29be324759ef89ac0520547438aec7939
Nazar <nazarkosandyak@gmail.com>
2021-07-05 21:22:59 +0300

    test

--- AnnouncementWebsite/server.js
+++ AnnouncementWebsite/server.js
@@ -9,15 +9,16 @@ const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const port = process.env.PORT || 3000;
 
+server.use(middlewares);
+server.use(router);
+server.listen(port);
+// server.listen(port)
 
 app.use(express.static('./dist/AnnouncementWebsite'));
 app.get('/*', (req, res) =>
     res.sendFile('index.html', {root: 'dist/AnnouncementWebsite/'}),
 );
-app.listen(process.env.PORT || 5000,server.listen(port));
+app.listen(process.env.PORT || 5000);
 
 
 
-server.use(middlewares);
-server.use(router);
-server.listen(port);
