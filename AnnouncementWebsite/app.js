47646bc3b874324f723c05df1c781ae47fe9a4b7
Nazar <nazarkosandyak@gmail.com>
2021-07-05 19:52:19 +0300

    edited app.js

--- AnnouncementWebsite/app.js
+++ AnnouncementWebsite/app.js
@@ -1,17 +1,6 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-const server = jsonServer.create()
-const router = jsonServer.router("./db.json")
-const middlelewares = jsonServer.defaults({
-    static:'./dist/AnnouncementWebsite'
-})
-server.use(middlelewares)
-server.use(jsonServer.rewriter({
-    '/api/*':'/$1',
-}))
-server.use(router)
-server.use(process.env.PORT || 3000)
 
 const app = express();
 
