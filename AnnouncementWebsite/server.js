1c4ca6d0abddf97c794469e11251f1f56a6bf90a
Nazar <nazarkosandyak@gmail.com>
2021-07-06 11:51:27 +0300

    test

--- AnnouncementWebsite/server.js
+++ AnnouncementWebsite/server.js
@@ -14,7 +14,6 @@
 
 const express = require('express');
 const path = require('path');
-const http = require('http')
 const app = express();
 
 const port = process.env.PORT || 5000
@@ -22,11 +21,4 @@ app.use(express.static(__dirname + '/dist/AnnouncementWebsite'));
 app.get('/*',(req,res)=>{'index.html',{root:'dist/AnnouncementWebsite/'}});
 app.listen(port)
 
-const jsonServer = require('json-server');
-const server = jsonServer.create()
-const router = jsonServer.router('db.json')
-const middlewares = jsonServer.defaults();
-server.use(middlewares)
-server.use(router)
-const serverPort = process.env.PORT || 3000
-server.listen(serverPort,()=>console.log('Running...'))
+
