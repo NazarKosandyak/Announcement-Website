53d89e338557ced4b2de2f936a19eb99b332d4c9
Nazar <nazarkosandyak@gmail.com>
2021-07-06 10:24:02 +0300

    test

--- AnnouncementWebsite/server.js
+++ AnnouncementWebsite/server.js
@@ -4,9 +4,22 @@ const path = require('path');
 const app = express();
 
 
-app.use(express.static('./dist/AnnouncementWebsite'));
-app.get('/*', (req, res) =>
-    res.sendFile('index.html', {root: 'dist/AnnouncementWebsite/'}),
+// app.use(express.static('./dist/AnnouncementWebsite'));
+// app.get('/*', (req, res) =>
+//     res.sendFile('index.html', {root: 'dist/AnnouncementWebsite/'}),
     
-);
-app.listen(process.env.PORT || 5000);
+// );
+// app.listen(process.env.PORT || 5000);
+  
+const jsonServer = require('./json-server');
+const server = jsonServer.create();
+const router = jsonServer.router('db.json');
+const middlewares = jsonServer.defaults();
+const port = process.env.PORT || 3000;
+
+server.use(middlewares);
+server.use(router);
+
+server.listen(port, () => {
+    console.log(`Test Custom Data API listening on port ${port}!`)
+});
