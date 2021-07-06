34abeb269784bc20c898a558ce898aa063cb0f28
Nazar <nazarkosandyak@gmail.com>
2021-07-06 21:56:55 +0300

    test

--- AnnouncementWebsite/server.js
+++ AnnouncementWebsite/server.js
@@ -3,9 +3,13 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = process.env.PORT || 5000
-app.use(express.static(__dirname + '/dist/AnnouncementWebsite'));
-app.get('/*',(req,res)=>{'index.html',{root:'dist/AnnouncementWebsite/'}});
-app.listen(port)
+// app.use(express.static('/dist/AnnouncementWebsite'));
+// app.get('/*',(req,res)=>{'index.html',{root:'dist/AnnouncementWebsite/'}});
+// app.listen(port)
 
 
+app.use(express.static('./dist/AnnouncementWebsite'));
+app.get('/*', (req, res) =>
+      res.sendFile('index.html', {root: 'dist/AnnouncementWebsite/'}),
+);
+app.listen(process.env.PORT || 5000);
